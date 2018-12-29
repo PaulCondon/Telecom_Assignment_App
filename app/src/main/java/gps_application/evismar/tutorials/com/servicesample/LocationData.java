efc6064c1dcf52dfe2f08736194cf414d892f71f
Paul Condon <paul.condon96@gmail.com>
2018-12-29 10:19:40 +0000

    First Commit of Sample Service code

---
+++ app/src/main/java/gps_application/evismar/tutorials/com/servicesample/LocationData.java
@@ -0,0 +1,23 @@
+package gps_application.evismar.tutorials.com.servicesample;
+
+/**
+ * Created by finnk on 11/5/2016.
+ */
+
+public class LocationData {
+
+    public double longitude;
+    public double latitude;
+
+    public LocationData() {
+
+    }
+
+    public LocationData(double lati,double longi) {
+        longitude = longi;
+        latitude = lati;
+    }
+
+
+
+}
